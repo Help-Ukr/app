7b55781be68bd2878bedd05733f1050ab6cb188e
Vlad <appako2603@gmail.com>
2022-04-02 15:35:51 +0300

    ComingSoon component

---
+++ components/ComingSoon/index.tsx
@@ -0,0 +1,11 @@
+import { Paper, Typography } from '@mui/material';
+
+const ComingSoon = () => {
+    return (
+        <Paper sx={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography variant="h4">Coming soon</Typography>
+        </Paper>
+    );
+};
+
+export default ComingSoon;
--- pages/transport.tsx
+++ pages/transport.tsx
@@ -1,8 +1,16 @@
 import PageLayout from '@cmts/PageLayout';
+import { Box } from '@mui/material';
 import type { NextPage } from 'next';
+import ComingSoon from '~/components/ComingSoon';
 
 const Transport: NextPage = () => {
-    return <PageLayout>Coming soon</PageLayout>;
+    return (
+        <PageLayout>
+            <Box sx={{ height: '100vh', width: '100vw', display: 'flex', position: 'absolute', top: 0 }}>
+                <ComingSoon />
+            </Box>
+        </PageLayout>
+    );
 };
 
 export default Transport;
